6aa0b103a22f57da93ff5640cdccc9db2802fd8c
Roman Berger <romanberger001@gmail.com>
2022-10-31 15:49:06 +0000

    task explaination

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/*
+ * a minigolf game in one dimension
+ *
+ * the target distance is randomly generated (40-60 m)
+ * each round a force is input by the player and the new distance is calculated
+ * the friction is a random number 0.25-0.75 generated each round
+ *
+ * when the game is done the user is asked if they want to play again
+ *
+ * ----------------------------------------------------------------------------------------------------------|
+ * Part 1:
+ *
+ * every round:
+ *  display state of the game
+ *  read input from player
+ *  calculate the new position
+ *  check if at the target (wiithin 1m of target)
+ *
+ *
+ *
+ */
 
 int main( int argv, char *argc[] )
 {
